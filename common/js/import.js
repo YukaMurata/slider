08d6bd318f883115ea2fe7123687daeaf858dbf5
murata-yuka <murata-yuka@9105-109520.local>
2016-12-10 20:57:40 +0900

    sliderファイル

--- common/js/import.js
+++
@@ -1,4 +0,0 @@
-document.write('<script src="/common/js/plugins/jquery-1.9.1.min.js" type="text/javascript"></script>');
-document.write('<script src="/common/js/jquery.easing.js" type="text/javascript"></script>');
-document.write('<script src="/common/js/common.js" type="text/javascript"></script>');
-document.write('<script src="/common/js/common2014.js" type="text/javascript"></script>');
